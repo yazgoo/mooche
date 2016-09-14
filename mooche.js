6e9502ce21b24eb2da8b9e610cd80708bcb8f3f3
yazgoo <yazgoo@gmail.com>
2016-09-14 22:54:52 +0200

    adding commit fetch for mma

--- mooche.js
+++ mooche.js
@@ -69,7 +69,7 @@ function line_to_mma_chords(line) {
     }
     return chords.join(" ");
 }
-var worker = new Worker("//rawgit.com/yazgoo/pypyjs-mma/3ee9da6503f9ab555efe28f24fbc44e7a0e222bd/worker.js");
+var worker = new Worker("//rawgit.com/yazgoo/pypyjs-mma/a0c0bcb034e9157300ad538d5628ef7a42082855/worker.js");
 // preload mma by running it once with an empty file
 worker.postMessage(["Groove Swing\n0 A7"]);
 worker.onmessage = function(e) {
